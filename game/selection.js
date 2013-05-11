94500f8a915d0c84337d9422fb6be9719ae04825
Grzegorz Tanczyk <fb@tanczyk.pl>
2013-05-11 14:43:37 +0200

    bugfix

--- game/selection.js
+++ game/selection.js
@@ -1,58 +1,62 @@
-var Selection = new (function() {
-	var filter = this.filter = new Deferred();;
-	var point = this.point = new Deferred();		
-	
-	var selection = [];
-			
-	view.on('contextmenu', function(event) {
-		point.resolve(event.clientX, event.layerY, event.eX, event.eY, selection);
-	});
-	
-	// unit selection
-	
-	var mouseMoved, mousePressed, pressX, pressY;
-	
-	filter.then(function(faction, check) {
-	
-		view.on('click', function(event) {
-			if(!mouseMoved && !world.query(event.eX, event.eY, 8).some(function(object) {
-				if(selection.indexOf(object)==-1 && object.opts.faction == faction)
-					selection.push((object.selected = true) && object);
-				return true;
-			}))
-				selection = selection.filter(function(object) {
-					object.selected = false;
-				});	
+var Selection;
+
+DomReady.ready(function() {
+	Selection = new (function() {
+		var filter = this.filter = new Deferred();;
+		var point = this.point = new Deferred();		
+		
+		var selection = [];
+				
+		view.on('contextmenu', function(event) {
+			point.resolve(event.clientX, event.layerY, event.eX, event.eY, selection);
 		});
 		
-		view.on('mousedown', function(event) {
-			mouseMoved = false;
-			mousePressed = true;
-			pressX = event.eX;
-			pressY = event.eY;
-		});		
+		// unit selection
 		
-		view.on('mouseup', function(event) {
-			mousePressed = false;
-		});		
+		var mouseMoved, mousePressed, pressX, pressY;
 		
-		view.on('mousemove', function(event) {
-			mouseMoved = true;
-			var rect = mousePressed && [pressX, pressY, event.eX, event.eY];
-			
-			if(mousePressed) {			
-				if(!world.query(event.eX, event.eY, 8, rect).every(function(object) {
+		filter.then(function(faction, check) {
+		
+			view.on('click', function(event) {
+				if(!mouseMoved && !world.query(event.eX, event.eY, 8).some(function(object) {
 					if(selection.indexOf(object)==-1 && object.opts.faction == faction)
-						selection.push((object.selected = true) && object);				
+						selection.push((object.selected = true) && object);
 					return true;
-				}))					
+				}))
 					selection = selection.filter(function(object) {
 						object.selected = false;
-					});							
-			} else
-				view.pointer(world.query(event.eX, event.eY, 8).length > 0);
+					});	
+			});
+			
+			view.on('mousedown', function(event) {
+				mouseMoved = false;
+				mousePressed = true;
+				pressX = event.eX;
+				pressY = event.eY;
+			});		
+			
+			view.on('mouseup', function(event) {
+				mousePressed = false;
+			});		
+			
+			view.on('mousemove', function(event) {
+				mouseMoved = true;
+				var rect = mousePressed && [pressX, pressY, event.eX, event.eY];
 				
+				if(mousePressed) {			
+					if(!world.query(event.eX, event.eY, 8, rect).every(function(object) {
+						if(selection.indexOf(object)==-1 && object.opts.faction == faction)
+							selection.push((object.selected = true) && object);				
+						return true;
+					}))					
+						selection = selection.filter(function(object) {
+							object.selected = false;
+						});							
+				} else
+					view.pointer(world.query(event.eX, event.eY, 8).length > 0);
+					
+			});
+		
 		});
-	
-	});
-})();
+	})();
+});
